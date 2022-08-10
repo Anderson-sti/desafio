5fab2235add2207b907e6ba080ce0cc925ff7b0c
Anderson Santiago <103945212+Anderson-sti@users.noreply.github.com>
2022-08-10 15:12:32 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let resultado = prompt("x=2 e y=5 \n x + Y = \n + \n x . y = \n RESULTADO? ")
 
 if (resultado == 17){
-    document.write("<head><link rel='stylesheet' href='index.css'</head><body><h1 class='one'>01010000 01000001 01010010 01000001 01000010 01000101 01001110 01010011</h1><img class='virus' src='virus.png'></body>")
+    document.write(`<head><link rel="stylesheet" href="index.css"></head><body><h1 class="one">01010000 01000001 01010010 01000001 01000010 01000101 01001110 01010011</h1><img class="virus" src="virus.png"></body>`)
 } else {
-    document.write("<head><link rel='stylesheet' href='index.css'</head><body><h1 class='one'>ERRADO MEU JOVEM, TENTE NOVAMENTE</h1><img class='virus' src='virus.png'></body>")
+    document.write(`<head><link rel="stylesheet" href="index.css"></head><body><h1 class="one">ERRADO MEU JOVEM, TENTE NOVAMENTE</h1><img class="virus" src="virus.png"></body>`)
 }
